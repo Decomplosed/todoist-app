1d25c1eb24dbf7a9b6d3475f52fffc66bc7bff5d
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-08 15:52:20 +0200

    created elements for sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,3 +1,32 @@
 import React from 'react';
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from 'react-icons/fa';
 
-export const Sidebar = () => <p>SIDEBAR</p>;
+export const Sidebar = () => (
+  <div className='sidebar' data-test-id='sidebar'>
+    <ul className='sidebar__generic'>
+      <li>
+        <span>
+          <FaInbox />
+        </span>
+        <span>Inbox</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendar />
+        </span>
+        <span>Today</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendarAlt />
+        </span>
+        <span>Next 7 days</span>
+      </li>
+    </ul>
+  </div>
+);
