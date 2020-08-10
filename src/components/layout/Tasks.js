3d2286e7cee9418a0d83b57eb6128fb066e41106
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-10 22:53:12 +0200

    created container for tasks data

--- src/components/layout/Tasks.js
+++ src/components/layout/Tasks.js
@@ -4,4 +4,10 @@ export const Tasks = () => {
   const tasks = [];
 
   let projectName = '';
+
+  return (
+    <div className='tasks' data-testid='tasks'>
+      <h2 data-testid='project-name'>{projectName}</h2>
+    </div>
+  );
 };
