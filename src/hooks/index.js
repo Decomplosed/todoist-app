1f8702aa07d53f87aca4e5159a5e0e20bd9ec9bc
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-10 22:38:00 +0200

    return function that calls unsubscribe

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -44,5 +44,7 @@ export const useTasks = (selectedProject) => {
 
       setArchivedTasks(newTasks.filter((task) => task.archived !== false));
     });
+
+    return () => unsubscribe();
   }, [selectedProject]);
 };
