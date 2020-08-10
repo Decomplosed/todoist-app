035f9c1fbf46cbb18c8ea79eb64abda5cd3fcfea
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-10 23:00:43 +0200

    created a jsx template for Checkbox

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -2,4 +2,14 @@ import { firebase } from '../firebase';
 
 export const Checkbox = ({ id }) => {
   firebase.firestore().collection('tasks').doc(id).update({ archived: true });
+
+  return (
+    <div
+      className='checkbox-holder'
+      data-testid='checkbox-action'
+      onClick={() => archiveTask()}
+    >
+      <span className='checkbox'></span>
+    </div>
+  );
 };
