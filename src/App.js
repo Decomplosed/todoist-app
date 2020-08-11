c96a52e32fab11f803257b1248b10c2aaaf0adc4
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-11 09:03:12 +0200

    imported in App.scss into App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 
+import './App.scss';
+
 export const App = () => (
   <div className='App'>
     <Header />
