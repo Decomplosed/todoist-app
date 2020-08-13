d4f84d2bba68f01d059dffa8602349075414e5b5
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-13 08:47:32 +0200

    created a useState case for activeValue

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,5 @@
 import React, { useState } from 'react';
 
-export const Projects = ({ activeNull = true }) => {};
+export const Projects = ({ activeValue = true }) => {
+  const [active, setActive] = useState(activeValue);
+};
