269c2be349c46b10279f256daea38b9f8ef575ad
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-13 08:27:52 +0200

    copied code from projects context and started adjusting to the selected project scenario

---
+++ src/context/selected-project-context.js
@@ -0,0 +1,15 @@
+import React, { createContext, useContext } from 'react';
+import { useProjects } from '../hooks';
+
+export const SelectedProjectContext = createContext();
+export const SelectedProjectProvider = ({ children }) => {
+  const { projects, setProjects } = useProjects();
+
+  return (
+    <ProjectsContext.Provider value={{ projects, setProjects }}>
+      {children}
+    </ProjectsContext.Provider>
+  );
+};
+
+export const useProjectsValue = () => useContext(ProjectsContext);
