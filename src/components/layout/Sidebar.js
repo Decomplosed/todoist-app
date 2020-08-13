46d3eca5246dde9735f9d662a3bb6aad31f4e909
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-13 08:41:59 +0200

    return the jsx inside block, not staight away

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -6,35 +6,37 @@ import {
   FaRegCalendar,
 } from 'react-icons/fa';
 
-export const Sidebar = () => (
-  <div className='sidebar' data-test-id='sidebar'>
-    <ul className='sidebar__generic'>
-      <li data-testid='inbox' className='inbox'>
+export const Sidebar = () => {
+  return (
+    <div className='sidebar' data-test-id='sidebar'>
+      <ul className='sidebar__generic'>
+        <li data-testid='inbox' className='inbox'>
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li data-testid='today' className='today'>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li data-testid='next_7' className='next_7'>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 days</span>
+        </li>
+      </ul>
+      <div className='sidebar__middle'>
         <span>
-          <FaInbox />
+          <FaChevronDown />
         </span>
-        <span>Inbox</span>
-      </li>
-      <li data-testid='today' className='today'>
-        <span>
-          <FaRegCalendar />
-        </span>
-        <span>Today</span>
-      </li>
-      <li data-testid='next_7' className='next_7'>
-        <span>
-          <FaRegCalendarAlt />
-        </span>
-        <span>Next 7 days</span>
-      </li>
-    </ul>
-    <div className='sidebar__middle'>
-      <span>
-        <FaChevronDown />
-      </span>
-      <h2>Projects</h2>
+        <h2>Projects</h2>
+      </div>
+      <ul className='sidebar__projects'>Projects will be here!</ul>
+      Add project component here!
     </div>
-    <ul className='sidebar__projects'>Projects will be here!</ul>
-    Add project component here!
-  </div>
-);
+  );
+};
