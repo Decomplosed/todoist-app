465cf78492c1715a360a2c0e835e38ee57de4738
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-13 08:30:13 +0200

    changed the name of the context component

--- src/context/selected-project-context.js
+++ src/context/selected-project-context.js
@@ -6,9 +6,9 @@ export const SelectedProjectProvider = ({ children }) => {
   const { selectedProject, setSelectedProject } = useState('INBOX');
 
   return (
-    <ProjectsContext.Provider value={{ projects, setProjects }}>
+    <SelectedProjectContext.Provider value={{ projects, setProjects }}>
       {children}
-    </ProjectsContext.Provider>
+    </SelectedProjectContext.Provider>
   );
 };
 
