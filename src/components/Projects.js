9245fa15f6f7ea5e85b8043fad6381849ea0405b
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-14 09:08:45 +0200

    loop through projectw an create a list item for each elements inside projects array

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,4 +5,8 @@ export const Projects = ({ activeValue = true }) => {
   const [active, setActive] = useState(activeValue);
   const { setSelectedProject } = useSelectedProjectValue();
   const { projects } = useProjectsValue();
+
+  return (
+    projects && projects.map((project) => <li key={project.projectId}></li>)
+  );
 };
