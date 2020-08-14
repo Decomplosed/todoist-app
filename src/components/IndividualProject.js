fbb7758581772d67f087b7e20923d74b9e979654
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-14 09:31:57 +0200

    imported firebase

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -1,3 +1,4 @@
 import React, { useState } from 'react';
 import { FaTrashIcon } from 'react-icons/fa';
 import { useProjectsValue, useSelectedProjectValue } from '../context';
+import { firebase } from '../firebase';
