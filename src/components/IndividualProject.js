1c38b13e5dbad9e85910a2a004b4a83e05d0c87f
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-14 09:32:28 +0200

    declared IndividualProject functional component

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -2,3 +2,5 @@ import React, { useState } from 'react';
 import { FaTrashIcon } from 'react-icons/fa';
 import { useProjectsValue, useSelectedProjectValue } from '../context';
 import { firebase } from '../firebase';
+
+export const IndividualProject = () => {}
