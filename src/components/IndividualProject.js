1b48c8ed16c473a53fead8f6377b86f54de7cf9f
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-14 09:46:43 +0200

    added delete button iinside modal

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -35,6 +35,12 @@ export const IndividualProject = ({ project }) => {
             <div className='project-delete-modal'>
               <div className='project-delete-modal__inner'>
                 <p>Are you sure you want to delete this project?</p>
+                <button
+                  type='button'
+                  onClick={() => deleteProject(project.docId)}
+                >
+                  Delete
+                </button>
               </div>
             </div>
           )}
