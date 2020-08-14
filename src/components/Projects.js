d49ad3ff13cbc49239f902032934b589fdb59716
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-14 09:07:11 +0200

    set selected project using useSelectedProjectValue hook

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,4 +3,5 @@ import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Projects = ({ activeValue = true }) => {
   const [active, setActive] = useState(activeValue);
+  const { setSelectedProject } = useSelectedProjectValue();
 };
