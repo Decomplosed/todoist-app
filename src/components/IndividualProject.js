bc252228cb10abafe6e22432f03838213dc3f814
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-14 09:41:32 +0200

    added project name

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -22,6 +22,7 @@ export const IndividualProject = ({ project }) => {
     return (
       <>
         <span className='sidebar__dot'>•</span>
+        <span className='sidebar__project-name'>{project.name}</span>
       </>
     );
   };
