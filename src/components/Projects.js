2c2b9d8381b7c87054d851fa0a4f9d964ea78029
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-14 09:06:37 +0200

    imported in useSelectedProjectValue, useProjectsValue

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Projects = ({ activeValue = true }) => {
   const [active, setActive] = useState(activeValue);
