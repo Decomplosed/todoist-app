750d222d0acfd768768a1dc37c0ed370a2e6d9ea
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-15 12:26:07 +0200

    fixed error with brackets

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -18,35 +18,35 @@ export const IndividualProject = ({ project }) => {
         setProjects([...projects]);
         setSelectedProject('INBOX');
       });
-
-    return (
-      <>
-        <span className='sidebar__dot'>•</span>
-        <span className='sidebar__project-name'>{project.name}</span>
-        <span
-          className='sidebar__project-delete'
-          data-testid='delete-project'
-          onClick={() => {
-            setShowConfirm(!showConfirm);
-          }}
-        >
-          <FaTrashAlt />
-          {showConfirm && (
-            <div className='project-delete-modal'>
-              <div className='project-delete-modal__inner'>
-                <p>Are you sure you want to delete this project?</p>
-                <button
-                  type='button'
-                  onClick={() => deleteProject(project.docId)}
-                >
-                  Delete
-                </button>
-                <span onClick={() => setShowConfirm(!showConfirm)}>Cancel</span>
-              </div>
-            </div>
-          )}
-        </span>
-      </>
-    );
   };
+
+  return (
+    <>
+      <span className='sidebar__dot'>•</span>
+      <span className='sidebar__project-name'>{project.name}</span>
+      <span
+        className='sidebar__project-delete'
+        data-testid='delete-project'
+        onClick={() => {
+          setShowConfirm(!showConfirm);
+        }}
+      >
+        <FaTrashAlt />
+        {showConfirm && (
+          <div className='project-delete-modal'>
+            <div className='project-delete-modal__inner'>
+              <p>Are you sure you want to delete this project?</p>
+              <button
+                type='button'
+                onClick={() => deleteProject(project.docId)}
+              >
+                Delete
+              </button>
+              <span onClick={() => setShowConfirm(!showConfirm)}>Cancel</span>
+            </div>
+          </div>
+        )}
+      </span>
+    </>
+  );
 };
