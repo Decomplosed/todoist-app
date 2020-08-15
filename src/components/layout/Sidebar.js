fb1d7c49f4cf84408c1d0e2d7e4cb1450b340162
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-15 12:46:43 +0200

    imported in AddProject

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -6,6 +6,7 @@ import {
   FaRegCalendar,
 } from 'react-icons/fa';
 import { Projects } from '../Projects';
+import { AddProject } from './AddProject';
 import { useSelectedProjectValue } from '../../context';
 
 export const Sidebar = () => {
