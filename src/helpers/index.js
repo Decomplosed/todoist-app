8e77d507fbc75efdb5c911cc0fae9e30d707d00b
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-15 13:11:16 +0200

    created getTitle helper function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,7 @@
 import { collatedTasks } from '../constants';
 
+export const getTitle = (projects, projectId) =>
+  projects.find((project) => project.projectId === projectId);
+
 export const collatedTasksExist = (selectedProject) =>
   collatedTasks.find((task) => task.key === selectedProject);
