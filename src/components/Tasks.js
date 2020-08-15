645ab52f26bc38770e05aa949bd158d4eb32c7e9
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-15 13:09:10 +0200

    get selectedProject and projects

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -6,6 +6,8 @@ import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
 import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Tasks = () => {
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
   const { tasks } = useTasks(selectedProject);
 
   let projectName = '';
