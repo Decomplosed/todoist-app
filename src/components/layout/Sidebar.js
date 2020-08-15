e2ee6d9f428c8454ad1dd2bd9c86db496ccfce90
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-15 12:47:05 +0200

    render AddProject instead of Project

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -43,7 +43,7 @@ export const Sidebar = () => {
         <h2>Projects</h2>
       </div>
       <ul className='sidebar__projects'>{showProjects && <Projects />}</ul>
-      {showProjects && <Projects />}
+      {showProjects && <AddProject />}
     </div>
   );
 };
