e0930c5991b6506f089d247958d91c80ba66f6eb
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-16 14:27:36 +0200

    imported useProjectsValue

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { firebase } from '../firebase';
 import { generatePushId } from '../helpers';
+import { useProjectsValue } from '../context';
 
 export const AddProject = () => <p>ADD_PROJECT_HERE</p>;
