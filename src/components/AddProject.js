1f2eff0cf975d1bb9427d1267a096f8cf1982736
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-16 14:28:39 +0200

    imported useState

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { firebase } from '../firebase';
 import { generatePushId } from '../helpers';
 import { useProjectsValue } from '../context';
