202e2a2002319540320873812a79bdba72320a9b
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-16 14:30:38 +0200

    generate projectId

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -6,4 +6,6 @@ import { useProjectsValue } from '../context';
 export const AddProject = ({ shouldShow = false }) => {
   const [show, setShow] = useState(shouldShow);
   const [projectName, setProjectName] = useState('');
+
+  const projectId = generatePushId()
 };
