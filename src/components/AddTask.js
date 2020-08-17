171de63e712f92e4ed2d4695678498f08d353d39
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-17 11:03:23 +0200

    imported useSelectedProjectValue

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,3 +2,4 @@ import React, { useState } from 'react';
 import { FaRegListAlt, FaRegCalendarAlt } from 'react-icon/fa';
 import moment from 'moment';
 import { firebase } from '../firebase';
+import { useSelectedProjectValue } from '../context';
