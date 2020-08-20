2065c2cdbbafff0e6af9459e15188c65825481c5
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-20 10:12:43 +0200

    created jsx body

--- src/components/TaskDate.js
+++ src/components/TaskDate.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) => (
-  <p>Hello</p>
-);
+export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) => showTaskDate &&  (
+  <div className="task-date" data-testid='task-date-overlay'>
+    <ul className="task-date__list">
+      <li onClick={}></li>
+    </ul>
+  </div>
+)
