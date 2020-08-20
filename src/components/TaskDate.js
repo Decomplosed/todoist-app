57069ac25878b214e9f6f33ae9fd1c21b38f3a74
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-20 10:29:02 +0200

    wrapped list item content inside div

--- src/components/TaskDate.js
+++ src/components/TaskDate.js
@@ -6,41 +6,47 @@ export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) =>
   showTaskDate && (
     <div className='task-date' data-testid='task-date-overlay'>
       <ul className='task-date__list'>
-        <li
-          onClick={() => {
-            setShowTaskDate(false);
-            setTaskDate(moment().format('DD/MM/YYYY'));
-          }}
-          data-testid='task-date-overlay'
-        >
-          <span>
-            <FaSpaceShuttle />
-            <span>Today</span>
-          </span>
+        <li>
+          <div
+            onClick={() => {
+              setShowTaskDate(false);
+              setTaskDate(moment().format('DD/MM/YYYY'));
+            }}
+            data-testid='task-date-overlay'
+          >
+            <span>
+              <FaSpaceShuttle />
+              <span>Today</span>
+            </span>
+          </div>
         </li>
-        <li
-          onClick={() => {
-            setShowTaskDate(false);
-            setTaskDate(moment().add(1, 'day').format('DD/MM/YYYY'));
-          }}
-          data-testid='task-date-tomorrow'
-        >
-          <span>
-            <FaSun />
-            <span>Tomorrow</span>
-          </span>
+        <li>
+          <div
+            onClick={() => {
+              setShowTaskDate(false);
+              setTaskDate(moment().add(1, 'day').format('DD/MM/YYYY'));
+            }}
+            data-testid='task-date-tomorrow'
+          >
+            <span>
+              <FaSun />
+              <span>Tomorrow</span>
+            </span>
+          </div>
         </li>
-        <li
-          onClick={() => {
-            setShowTaskDate(false);
-            setTaskDate(moment().add(7, 'days').format('DD/MM/YYYY'));
-          }}
-          data-testid='task-date-next-week'
-        >
-          <span>
-            <FaRegPaperPlane />
-            <span>Next week</span>
-          </span>
+        <li>
+          <div
+            onClick={() => {
+              setShowTaskDate(false);
+              setTaskDate(moment().add(7, 'days').format('DD/MM/YYYY'));
+            }}
+            data-testid='task-date-next-week'
+          >
+            <span>
+              <FaRegPaperPlane />
+              <span>Next week</span>
+            </span>
+          </div>
         </li>
       </ul>
     </div>
