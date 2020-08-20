7893e2a0c516aa138298dcb51c6685050b41a978
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-20 10:13:54 +0200

    fixed error

--- src/components/TaskDate.js
+++ src/components/TaskDate.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) => showTaskDate &&  (
-  <div className="task-date" data-testid='task-date-overlay'>
-    <ul className="task-date__list">
-      <li onClick={
-        setShowTaskDate(false)
-        setTaskDate()
-      }></li>
-    </ul>
-  </div>
-)
+export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) =>
+  showTaskDate && (
+    <div className='task-date' data-testid='task-date-overlay'>
+      <ul className='task-date__list'>
+        <li
+          onClick={() => {
+            setShowTaskDate(false);
+            setTaskDate();
+          }}
+        ></li>
+      </ul>
+    </div>
+  );
