35aaaf6b90400a19c42336f0287eafb71e867777
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-20 10:32:06 +0200

    extract darkMode, setDarkMode from props

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaPizzaSlice } from 'react-icons/fa';
 
-export const Header = () => {
+export const Header = ({ darkMode, setDarkMode }) => {
   return (
     <header className='header' data-testid='header'>
       <nav>
