deb6968fbc9d929317c1c5d317d957bce0b4a5a2
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-20 10:13:22 +0200

    call setShowTaskDate on click

--- src/components/TaskDate.js
+++ src/components/TaskDate.js
@@ -3,7 +3,9 @@ import React from 'react';
 export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) => showTaskDate &&  (
   <div className="task-date" data-testid='task-date-overlay'>
     <ul className="task-date__list">
-      <li onClick={}></li>
+      <li onClick={
+        setShowTaskDate(false)
+      }></li>
     </ul>
   </div>
 )
