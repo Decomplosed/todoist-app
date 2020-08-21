6b42d0e9cc8d300a977025d7ff9fe244a03cfc9d
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-21 10:56:48 +0200

    improved accessibility inside Checkbox component

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -11,8 +11,14 @@ export const Checkbox = ({ id, taskDesc }) => {
       className='checkbox-holder'
       data-testid='checkbox-action'
       onClick={() => archiveTask()}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') archiveTask();
+      }}
+      aria-label={`Mark ${taskDesc} as done?`}
+      role='button'
+      tabIndex={0}
     >
-      <span className='checkbox'></span>
+      <span className='checkbox' />
     </div>
   );
 };
