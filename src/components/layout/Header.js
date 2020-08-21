ac73c460bafd084d02fee486d7e08791875784aa
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-21 10:58:41 +0200

    passed shouldShowMain instead of setShouldShowMain

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -43,7 +43,7 @@ export const Header = ({ darkMode, setDarkMode }) => {
 
       <AddTask
         showAddTaskMain={false}
-        setShouldShowMain={setShouldShowMain}
+        shouldShowMain={shouldShowMain}
         showQuickAddTask={showQuickAddTask}
         setShowQuickAddTask={setShowQuickAddTask}
       />
