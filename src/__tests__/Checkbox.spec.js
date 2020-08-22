e4059cdaf9cda0546c922dc2c1f049d32759673a
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-22 19:13:05 +0200

    passed id to Checkbox

--- src/__tests__/Checkbox.spec.js
+++ src/__tests__/Checkbox.spec.js
@@ -19,7 +19,7 @@ jest.mock('../firebase', () => ({
 describe('<Checkbox />', () => {
   describe('Success', () => {
     it('renders the task checkbox', () => {
-      const { queryByTestId } = render(<Checkbox />);
+      const { queryByTestId } = render(<Checkbox id='1' />);
     });
   });
 });
