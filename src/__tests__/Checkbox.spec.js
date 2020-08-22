84950020d62ce5a5592bcdea697c9c569449b884
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-22 19:14:07 +0200

    extracted debug

--- src/__tests__/Checkbox.spec.js
+++ src/__tests__/Checkbox.spec.js
@@ -19,7 +19,9 @@ jest.mock('../firebase', () => ({
 describe('<Checkbox />', () => {
   describe('Success', () => {
     it('renders the task checkbox', () => {
-      const { queryByTestId } = render(<Checkbox id='1' taskDesc='Test description' />);
+      const { queryByTestId, debug } = render(
+        <Checkbox id='1' taskDesc='Test description' />,
+      );
     });
   });
 });
