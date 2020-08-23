fc43bdaaa18ee5ff23d49a2a9b199dbddcc4dd69
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-23 09:24:59 +0200

    imported useSelectedProjectValue from context

--- src/__tests__/AddTask.spec.js
+++ src/__tests__/AddTask.spec.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import { AddTask } from '../components/AddTask';
 import { firebase } from '../firebase';
+import { useSelectedProjectValue } from '../context';
 
 beforeEach(cleanup);
