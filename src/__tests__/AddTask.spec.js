93903e104c28998241a18c238efaf0f70c80bdb1
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-25 17:30:07 +0200

    formatting fix

--- src/__tests__/AddTask.spec.js
+++ src/__tests__/AddTask.spec.js
@@ -89,10 +89,7 @@ describe('<AddTask />', () => {
       const showQuickAddTask = true;
       const setShowQuickAddTask = jest.fn(() => !showQuickAddTask);
       const { queryByTestId } = render(
-        <AddTask
-          setShowQuickAddTask={setShowQuickAddTask}
-          showQuickAddTask
-        />,
+        <AddTask setShowQuickAddTask={setShowQuickAddTask} showQuickAddTask />,
       );
 
       fireEvent.click(queryByTestId('show-main-action'));
