0b3f997f354748fb99db75d2af4e0d0265ae12e6
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-26 08:36:03 +0200

    passed showMain

--- src/__tests__/AddTask.spec.js
+++ src/__tests__/AddTask.spec.js
@@ -184,7 +184,7 @@ describe('<AddTask />', () => {
       useSelectedProjectValue.mockImplementation(() => ({
         selectedProject: '1',
       }));
-      const { queryByTestId } = render(<AddTask />);
+      const { queryByTestId } = render(<AddTask showMain />);
     });
   });
 });
