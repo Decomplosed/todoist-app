e8385a87a8d4b614cfc9156fff6d8db1cae1a52b
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-28 08:44:08 +0200

    imported useSelectedProjectValue

--- src/__tests__/ProjectOverlay.spec.js
+++ src/__tests__/ProjectOverlay.spec.js
@@ -1,3 +1,4 @@
 import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import { ProjectOverlay } from '../components/ProjectOverlay';
+import { useSelectedProjectValue } from '../context';
