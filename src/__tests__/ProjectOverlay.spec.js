e8e5518f9cded0be1b78135129f5d2ba579b28a7
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-28 08:44:31 +0200

    cleanup +beforeEach

--- src/__tests__/ProjectOverlay.spec.js
+++ src/__tests__/ProjectOverlay.spec.js
@@ -2,3 +2,5 @@ import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import { ProjectOverlay } from '../components/ProjectOverlay';
 import { useSelectedProjectValue } from '../context';
+
+beforeEach(cleanup);
