8f8d3f4d8a3adb5bc21fde630acb1a9c61c32e4e
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-29 07:43:17 +0200

    mock the context

--- src/__tests__/Projects.spec.js
+++ src/__tests__/Projects.spec.js
@@ -3,3 +3,5 @@ import { render, fireEvent, cleanup } from '@testing-library/react';
 import { Projects } from '../components/Projects';
 
 beforeEach(cleanup);
+
+jest.mock('../context', () => ({}));
