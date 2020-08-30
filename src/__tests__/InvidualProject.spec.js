9635a7ae017d8129ffe2aff3aab36b4b2ddef7c7
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-30 08:40:14 +0200

    cleanup before each test

--- src/__tests__/InvidualProject.spec.js
+++ src/__tests__/InvidualProject.spec.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import { IndividualProject } from '../components/IndividualProject';
 
+beforeEach(cleanup);
+
 jest.mock('../context', () => ({}));
