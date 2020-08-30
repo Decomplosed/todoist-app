a42675b30978d6cff8969f8fa7a905584376ba79
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-30 08:39:40 +0200

    mock contect

--- src/__tests__/InvidualProject.spec.js
+++ src/__tests__/InvidualProject.spec.js
@@ -1,3 +1,5 @@
 import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import { IndividualProject } from '../components/IndividualProject';
+
+jest.mock('../context', () => ({}));
