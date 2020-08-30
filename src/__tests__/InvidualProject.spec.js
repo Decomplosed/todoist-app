47647a83d1d03f8599d69ea1dc2e2314ea01e9b5
Bart Frackiewicz <decomplosed@gmail.com>
2020-08-30 08:37:47 +0200

    imported in firebase

--- src/__tests__/InvidualProject.spec.js
+++ src/__tests__/InvidualProject.spec.js
@@ -1,3 +1,4 @@
 import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import { useProjectsValue } from '../context';
+import { firebase } from '../firebase';
