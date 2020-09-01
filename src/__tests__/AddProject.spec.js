02e72949c6052708ec314d66c15aebc29abb71f9
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-01 10:17:46 +0200

    mock context

--- src/__tests__/AddProject.spec.js
+++ src/__tests__/AddProject.spec.js
@@ -1,3 +1,5 @@
 import React from 'react';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import { AddProject } from '../components/AddProject';
+
+jest.mock('../context', () => ({}))
