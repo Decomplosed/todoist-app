b15e76b3d28a4df14c16026e77f705970c5bc29a
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-01 10:19:41 +0200

    passed 🙌 THE OFFICE ptoject

--- src/__tests__/AddProject.spec.js
+++ src/__tests__/AddProject.spec.js
@@ -5,6 +5,13 @@ import { AddProject } from '../components/AddProject';
 jest.mock('../context', () => ({
   useSelectedProjectValue: jest.fn(),
   useProjectsValue: jest.fn(() => ({
-    projects: [],
+    projects: [
+      {
+        name: '🙌 THE OFFICE',
+        projectId: '1',
+        userId: 'Jx3X378u2QjdGeEQdasj',
+        docId: 'michael-scott',
+      },
+    ],
   })),
 }));
