9d0a5838d2e9815f8643f48f41f81527a82ec210
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-01 10:17:59 +0200

    mock useSelectedProjectValue

--- src/__tests__/AddProject.spec.js
+++ src/__tests__/AddProject.spec.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import { AddProject } from '../components/AddProject';
 
-jest.mock('../context', () => ({}))
+jest.mock('../context', () => ({
+  useSelectedProjectValue: jest.fn(),
+}));
