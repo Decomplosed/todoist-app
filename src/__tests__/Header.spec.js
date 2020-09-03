920cfb36a482ceba4b6068fd1db53281ba0d46c0
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-03 17:16:23 +0200

    mock useSelectedProjectValue

--- src/__tests__/Header.spec.js
+++ src/__tests__/Header.spec.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import { Header } from '../components/layout/Header';
 
-jest.mock('../context', () => ({}));
+jest.mock('../context', () => ({
+  useSelectedProjectValue: jest.fn(() => ({})),
+}));
