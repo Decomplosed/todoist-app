42824f7521f90f7dea43c60b89bbba2c6dcebda8
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-03 17:15:59 +0200

    mock context

--- src/__tests__/Header.spec.js
+++ src/__tests__/Header.spec.js
@@ -1,3 +1,5 @@
 import React from 'react';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import { Header } from '../components/layout/Header';
+
+jest.mock('../context', () => ({}));
