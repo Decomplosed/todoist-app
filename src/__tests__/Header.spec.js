23c23c473996d8253f3fa93a2e504baa3d6fd92e
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-03 17:16:37 +0200

    passed selectedProject value

--- src/__tests__/Header.spec.js
+++ src/__tests__/Header.spec.js
@@ -3,5 +3,5 @@ import { render, cleanup, fireEvent } from '@testing-library/react';
 import { Header } from '../components/layout/Header';
 
 jest.mock('../context', () => ({
-  useSelectedProjectValue: jest.fn(() => ({})),
+  useSelectedProjectValue: jest.fn(() => ({ selectedProject: 1 })),
 }));
